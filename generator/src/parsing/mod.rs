194ffe0bc513ec76e0a22d5b783feb8247a7b2fe
MatrixSenpai <math.matrix@icloud.com>
2024-03-11 00:06:11 -0500

    merging develop fixes

--- generator/src/parsing/mod.rs
+++ generator/src/parsing/mod.rs
@@ -4,17 +4,43 @@ mod import;
 mod attribute;
 mod field;
 
-use sw4rm_rs::*;
+use sw4rm_rs::{
+    *, shared::*, openapi_v2::*, openapi_v3_0::*,
+};
 
-pub struct GenerationOptions {
-    pub parse_models: bool,
-    pub parse_apis: bool,
+pub struct GenerationOptions;
+struct Representation;
+
+pub fn parse_spec(spec: Spec, options: Option<GenerationOptions>) {
+    let spec_title = spec.info.title.clone();
+    info!("Parsing schema {spec_title}");
+
+    // TODO: maybe use flat_map here...?
+    spec.schemas().iter()
+        .map(|(key, schema)| resolve(&spec, key, schema))
+        .map(|(key, schema)| parse_definitions(&spec, key, schema));
 }
 
-pub fn parse_spec(spec: Spec, options: GenerationOptions) {
-
+// TODO: this should return Option instead
+fn resolve(spec: &Spec, key: &String, reference: &RefOr<Schema>) -> (String, Schema) {
+    debug!("RESOLUTION: {key}");
+    (
+        key.clone(),
+        reference.resolve(spec).unwrap(),
+    )
 }
 
-struct Representation {
+// TODO: this should return Option instead
+fn parse_definitions(spec: &Spec, definition_key: String, schema_item: Schema) {
+    match schema_item.schema_type.unwrap() {
+        // SchemaType::Object => parse_schema_object(spec, definition_key, schema_item),
+        _ => ()
+    };
+}
 
+fn parse_schema_object(spec: &Spec, definition_key: String, schema_item: Schema) {
+    info!("Parsing model for {}", &definition_key);
+
+    let required = schema_item.required;
+    let properties = schema_item.properties;
 }
--- generator/tests/test_reading.rs
+++ generator/tests/test_reading.rs
@@ -1,21 +1,51 @@
 use sw4rm_rs_generation::*;
 
-#[test]
-fn read_swagger_v2_yaml() {
-    _ = parse_file("../resources/swagger_2_0.yaml").unwrap();
+#[cfg(test)]
+#[ctor::ctor]
+fn initialize() {
+    std::env::set_var("RUST_LOG", "debug");
+    pretty_env_logger::init_timed()
 }
 
 #[test]
-fn read_swagger_v2_json() {
-    _ = parse_file("../resources/riot-swaggerspec-2.0.json").unwrap();
-}
+fn test_spec_parsing() {
+    let openapi_spec = sw4rm_rs::from_path("../resources/openapi_3_0.yaml").unwrap();
+    sw4rm_rs_generation::parsing::parse_spec(openapi_spec, None);
+    
+    let riot_spec = sw4rm_rs::from_path("../resources/riot-swaggerspec-2.0.json").unwrap();
+    sw4rm_rs_generation::parsing::parse_spec(riot_spec, None);
 
-#[test]
-fn read_openapi_v3_yaml() {
-    _ = parse_file("../resources/openapi_3_0.yaml").unwrap();
-}
+    let bungie_spec = match sw4rm_rs::from_path("../resources/bungie-spec.json") {
+        Ok(val) => val,
+        Err(e) => {
+            if let sw4rm_rs::error::Error::YamlError(e) = &e {
+                println!("location: {:?}", e.location().unwrap());
+            };
+            panic!("{:?}", e);
+        }
+    };
+    sw4rm_rs_generation::parsing::parse_spec(bungie_spec, None);
 
-#[test]
-fn read_openapi_v3_json() {
-    _ = parse_file("../resources/riot-openapi-3.0.0.json").unwrap();
+    let discord_spec = sw4rm_rs::from_path("../resources/discord-spec.json").unwrap();
+    sw4rm_rs_generation::parsing::parse_spec(discord_spec, None);
 }
+//
+// #[test]
+// fn read_swagger_v2_yaml() {
+//     _ = parse_file("../resources/swagger_2_0.yaml").unwrap();
+// }
+//
+// #[test]
+// fn read_swagger_v2_json() {
+//     _ = parse_file("../resources/riot-swaggerspec-2.0.json").unwrap();
+// }
+//
+// #[test]
+// fn read_openapi_v3_yaml() {
+//     _ = parse_file("../resources/openapi_3_0.yaml").unwrap();
+// }
+//
+// #[test]
+// fn read_openapi_v3_json() {
+//     _ = parse_file("../resources/riot-openapi-3.0.0.json").unwrap();
+// }
